d8e1a747ce8bf96c3b5116e5ec517b0a52527096
Samuel Borges <samuelmordred@gmail.com>
2017-11-26 16:54:29 -0200

    segunda tentativa de auto balanceamento avl

--- testavl.c
+++ testavl.c
@@ -4,9 +4,105 @@
 
 typedef struct node{
   char palavra[45];
+  int altura;
   struct node* left;
   struct node* right;
 }Node;
+
+int checkAltura(Node* raiz){
+  int altDir, altEsq;
+  if(raiz==NULL){
+    return 0;
+  }
+  if(raiz->left == NULL){
+    altEsq = 0;
+  }
+  else{
+    altEsq = 1 + raiz->left->altura;
+  }
+  if(raiz->right == NULL){
+    altDir = 0;
+  }
+  else{
+    altDir = 1 + raiz->right->altura;
+  }
+  if(altEsq>altDir){
+    return altEsq;
+  }
+  else{
+    return altDir;
+  }
+}
+
+int checkFB(Node* raiz){
+  int altDir, altEsq;
+  if(raiz==NULL){
+    return 0;
+  }
+  if(raiz->left == NULL){
+    altEsq = 0;
+  }
+  else{
+    altEsq = 1 + raiz->left->altura;
+  }
+  if(raiz->right == NULL){
+    altDir = 0;
+  }
+  else{
+    altDir = 1 + raiz->right->altura;
+  }
+    return altEsq-altDir;
+}
+
+Node * rotDir(Node *x)
+{
+    Node *y;
+    y=x->left;
+    x->left=y->right;
+    y->right=x;
+    x->altura = checkAltura(x);
+    y->altura = checkAltura(y);
+    return(y);
+}
+
+Node * rotEsq(Node *x)
+{
+    Node *y;
+    y=x->right;
+    x->right=y->left;
+    y->left=x;
+    x->altura = checkAltura(x);
+    y->altura = checkAltura(y);
+
+    return(y);
+}
+
+Node * RR(Node *T)
+{
+    T=rotEsq(T);
+    return(T);
+}
+
+Node * LL(Node *T)
+{
+    T=rotDir(T);
+    return(T);
+}
+
+Node * LR(Node *T)
+{
+    T->left=rotEsq(T->left);
+    T=rotDir(T);
+
+    return(T);
+}
+
+Node * RL(Node *T)
+{
+    T->right=rotDir(T->right);
+    T=rotEsq(T);
+    return(T);
+}
 
 int compare(char *str1, char *str2){
   while ( *str1 != '\0' && *str1 == *str2 )
@@ -26,23 +122,40 @@ Node* insert(Node* root, char *nome){
     novo->left = NULL;
     novo->right = NULL;
     root = novo;
-    return root;
   }
   else{
     if(compare(nome,root->palavra)>0){
       root->left = insert(root->left, nome);
+      if(checkFB(root)==2){
+        if(compare(nome,root->left->palavra)>0){
+          root=RR(root);
+        }
+        else{
+          root=RL(root);
+        }
+      }
     }
     else if(compare(nome,root->palavra)<0){
       root->right = insert(root->right, nome);
+      if(checkFB(root)==-2){
+        if(compare(nome,root->left->palavra)<0){
+          root=LL(root);
+        }
+        else{
+          root=LR(root);
+        }
+      }
     }
   }
+  root->altura = checkAltura(root);
+  return root;
 }
 
 Node *imprimeArvore(Node *raiz){
   if(raiz->right != NULL){
     raiz->right = imprimeArvore(raiz->right);
   }
-  printf("%s ", raiz->palavra);
+  printf("%s.%d ", raiz->palavra, raiz->altura);
   if(raiz->left != NULL){
     raiz->left = imprimeArvore(raiz->left);
   }
